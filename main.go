dc19179fc682508c3d175fe0bf9e9ccae35c669f
Bgoodwin24 <bgoodwin24@gmail.com>
2025-03-21 18:12:39 -0700

    feat: Implement Decoupling of presentation and parsing
    - Store current line in a variable
    - Split 8 byte chunks on `\n`  to create parts
    - Print each part except the last in `read: LINE` format
    - Add the last part to current line until reaching EOF
    - If anything is left in current line print in the same format

--- main.go
+++ main.go
@@ -4,6 +4,7 @@ import (
 	"fmt"
 	"io"
 	"os"
+	"strings"
 )
 
 func main() {
@@ -13,18 +14,28 @@ func main() {
 	}
 
 	size := make([]byte, 8)
+	LINE := ""
 
 	for {
 		n, err := msg.Read(size)
 		if err != nil {
 			if err == io.EOF {
+				fmt.Printf("read: %s\n", LINE)
 				os.Exit(0)
 			}
 			fmt.Printf("error reading file: %v", err)
 			break
 		}
 
-		fmt.Printf("read: %s\n", string(size[:n]))
+		parts := strings.Split(string(size[:n]), "\n")
+
+		for i := 0; i < len(parts)-1; i++ {
+			LINE += parts[i]
+			fmt.Printf("read: %s\n", LINE)
+			LINE = ""
+		}
+
+		LINE += parts[len(parts)-1]
 	}
 
 }
