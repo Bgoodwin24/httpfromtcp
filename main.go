5e239ca708b5c4e6695f46a99131297e341e4a39
Bgoodwin24 <bgoodwin24@gmail.com>
2025-03-21 17:52:06 -0700

    feat: Update main
    - Remove print statement and replace it by reading messages.txt 8 bytes at a time and print it in 8 byte chunks to stdout

--- main.go
+++ main.go
@@ -2,8 +2,29 @@ package main
 
 import (
 	"fmt"
+	"io"
+	"os"
 )
 
 func main() {
-	fmt.Println("I hope I get the job!")
+	msg, err := os.Open("./messages.txt")
+	if err != nil {
+		fmt.Printf("error opening file: %v", err)
+	}
+
+	size := make([]byte, 8)
+
+	for {
+		n, err := msg.Read(size)
+		if err != nil {
+			if err == io.EOF {
+				os.Exit(0)
+			}
+			fmt.Printf("error reading file: %v", err)
+			break
+		}
+
+		fmt.Printf("read: %s\n", string(size[:n]))
+	}
+
 }
