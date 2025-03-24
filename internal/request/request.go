8a61ca405ab05829a4e11e3ad4ad9c040ff2c213
Bgoodwin24 <bgoodwin24@gmail.com>
2025-03-23 19:02:39 -0700

    feat: Update Request
    - Create `Request` and `RequestLine` structs to represent HTTP requests
    - Implement `RequestFromReader` to:
        - Validate request structure
        - Transform data from `TCP -> HTTP`
        - Return a formatted `Request` object
    
    - Add unit tests to validate parsing logic, including edge cases

--- internal/request/request.go
+++ internal/request/request.go
@@ -1,1 +1,61 @@
 package request
+
+import (
+	"fmt"
+	"io"
+	"strings"
+	"unicode"
+)
+
+type Request struct {
+	RequestLine RequestLine
+}
+
+type RequestLine struct {
+	HttpVersion   string
+	RequestTarget string
+	Method        string
+}
+
+func RequestFromReader(reader io.Reader) (*Request, error) {
+	r, err := io.ReadAll(reader)
+	if err != nil {
+		return nil, err
+	}
+
+	if len(r) == 0 {
+		return nil, fmt.Errorf("received empty HTTP input")
+	}
+
+	split := strings.Split(string(r), "\r\n")
+
+	parts := strings.Fields(split[0])
+	if len(parts) != 3 {
+		return nil, fmt.Errorf("invalid request-line, expected 3 parts but got %d", len(parts))
+	}
+
+	method := parts[0]
+	request := parts[1]
+	version := parts[2]
+
+	for _, char := range method {
+		if !unicode.IsUpper(char) || !unicode.IsLetter(char) {
+			return nil, fmt.Errorf("selection is not the HTTP method: %s", method)
+		}
+	}
+
+	if version != "HTTP/1.1" {
+		return nil, fmt.Errorf("unsupported HTTP version: %s", version)
+	}
+
+	numericHTTPVersion := strings.TrimPrefix(version, "HTTP/")
+
+	result := &Request{
+		RequestLine: RequestLine{
+			Method:        method,
+			RequestTarget: request,
+			HttpVersion:   numericHTTPVersion,
+		},
+	}
+	return result, nil
+}
